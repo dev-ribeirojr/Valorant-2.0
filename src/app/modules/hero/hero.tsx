7615cbaaee605afaf78c74b0ea78edfd6cc0f2e3
Pablo Alves <ribeirojr1522@gmail.com>
2024-04-27 18:56:11 -0300

    styles: change  video size

--- src/app/modules/hero/hero.tsx
+++ src/app/modules/hero/hero.tsx
@@ -3,7 +3,12 @@ import LogoValorant from '@/assets/images/valorant-logo-text.svg'
 export function Hero() {
   return (
     <div className="mt-[56px] w-full relative ">
-      <video autoPlay muted loop className="object-cover w-full min-h-[300px]">
+      <video
+        autoPlay
+        muted
+        loop
+        className="object-cover w-full min-h-[300px] max-h-[600px]"
+      >
         <source src="/videos/Apresentation.mp4" type="video/mp4" />
       </video>
       <div className="absolute top-0 right-0 bottom-0 left-0 flex items-center justify-center gap-4 flex-col p-4">
