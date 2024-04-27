87f14fcc085db9290154d01aade8c37eb375b24a
Pablo Alves <ribeirojr1522@gmail.com>
2024-04-27 18:57:26 -0300

    feat: :sparkles: created about section

---
+++ src/components/aboutSection/aboutSection.tsx
@@ -0,0 +1,15 @@
+interface AboutSectionProps {
+  title: string
+  description: string
+}
+
+export function AboutSection({ title, description }: AboutSectionProps) {
+  return (
+    <div className="text-white mb-8">
+      <h2 className="text-5xl uppercase font-bold my-6 lg:text-7xl lg:my-8">
+        {title}
+      </h2>
+      <p className="text-white/80">{description}</p>
+    </div>
+  )
+}
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,2 +1,3 @@
 export { Header } from './header/header'
 export { Container } from './container/container'
+export { AboutSection } from './aboutSection/aboutSection'
