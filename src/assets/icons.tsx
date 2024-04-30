16f094f1b4b8f3ad3ef47d77394bff15f088e371
Pablo Alves <ribeirojr1522@gmail.com>
2024-04-29 23:18:16 -0300

    feat: :sparkles: instance icons

---
+++ src/assets/icons.tsx
@@ -0,0 +1,10 @@
+import { FaSearch } from 'react-icons/fa'
+import { FaCaretLeft, FaCaretRight } from 'react-icons/fa6'
+import { BiLoaderCircle } from 'react-icons/bi'
+
+export const iconsApp = {
+  search: <FaSearch />,
+  arrowLeft: <FaCaretLeft />,
+  arrowRight: <FaCaretRight />,
+  loading: <BiLoaderCircle />,
+}
