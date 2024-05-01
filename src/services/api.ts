177816527cd72160178785178a47c4c94862962f
Pablo Alves <ribeirojr1522@gmail.com>
2024-05-01 12:29:29 -0300

    feat: :sparkles: add return api translated pt-BR

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,5 +1,5 @@
 export function api(path: string, init?: RequestInit) {
-  const baseUrl = `${process.env.NEXT_PUBLIC_BASE_URL}${path}`
+  const baseUrl = `${process.env.NEXT_PUBLIC_BASE_URL}${path}?language=pt-BR`
   const url = new URL(baseUrl)
   return fetch(url, init)
 }
